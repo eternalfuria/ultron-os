610745c8a09852bd0a718349301e1be8c5b0c51c
eternalfuria <flavius5693@gmail.com>
2026-03-17 18:38:12 +0100

    Implement Memory class with remember and recall methods

---
+++ memory/memory.py
@@ -0,0 +1,7 @@
+class Memory:
+    def __init__(self):
+        self.data = {}
+    def remember(self, key, value):
+        self.data[key] = value
+    def recall(self, key):
+        return self.data.get(key, None)
